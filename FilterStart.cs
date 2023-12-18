824cf06afc5c3cf2b22c0daf74cb3316dc1cd882
zservirus <zservirus@yandex.ru>
2023-12-18 09:06:33 +0500

    Привели табуляции и оформление в норм вид.

---
+++ FilterStart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RowsFilter
+{
+    static public class FilterStart
+    {
+        /// <summary>
+        /// Метод реализует наполнение нового массива, из элементов заданного массива отобранных по критерию.
+        /// </summary>
+        /// <param name="arrayRow"> массив заданный пользователем</param>
+        /// <returns><param name="arrayFilter"> новый массив возвращаемый в основную программу</param></returns>
+        static public string[] Filter(string[] arrayRow)
+        {
+            int count = 0;
+            for (int i = 0; i < arrayRow.Length; i++)
+            {
+                if (arrayRow[i].Length <= 3 ){ count = count + 1; }
+            }
+            string[] arrayFilter = new string [count];
+            int indexArrayFilter = 0;
+            for (int indexArrayRow = 0; indexArrayRow < arrayRow.Length; indexArrayRow++)
+            {
+                if (arrayRow[indexArrayRow].Length <= 3)
+                {
+                    arrayFilter[indexArrayFilter] = arrayRow[indexArrayRow];
+                    indexArrayFilter++; 
+                }
+            }
+            return arrayFilter;
+        }
+    }
+}
---
+++ RowsFilter.cs
@@ -0,0 +1,39 @@
+﻿// Программа фильрование строк
+//using FilterStart;
+
+namespace RowsFilter
+{
+    public static class RowsFilter
+    {
+        public static void Main()
+        {
+            int size = 10;
+            int min = 0;
+            int max = 10000;
+            string[] myrows = CreateArray(size, min, max);
+            Console.WriteLine("Из cгенерированного массива строк, формирует новый массив из строк, длина которых меньше, либо равна 3 символам.");
+            PrintArray(myrows);
+            Console.Write(" -> ");
+            PrintArray(FilterStart.Filter(myrows));
+            Console.ReadKey();
+        }
+        //Метод генерирует новый масив с заданными параметрами
+        public static string[] CreateArray(int size, int min = 0, int max = 100000)
+        {
+            string[] newArray = new string[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = rnd.Next(min, max).ToString();
+            }
+            return newArray;
+        }
+        
+        // Метод вывода массива на экран в консоль
+        public static void PrintArray(string[] array)
+        {
+        Console.Write($"[{ string.Join(",", array)}]");
+        }
+    }
+}
+
