8a1666a056bbfce9bae4d90d98a1a6b5025dc520
zservirus <zservirus@yandex.ru>
2023-12-18 10:52:18 +0500

    Добавил возможность выбора размера и наполнения массива пользователем

--- RowsFilter.cs
+++ RowsFilter.cs
@@ -1,6 +1,9 @@
 ﻿// Программа фильрование строк
 //using FilterStart;
 
+using System;
+using System.Runtime.ExceptionServices;
+
 namespace RowsFilter
 {
     public static class RowsFilter
@@ -10,7 +13,27 @@ namespace RowsFilter
             int size = 10;
             int min = 0;
             int max = 10000;
-            string[] myrows = CreateArray(size, min, max);
+            Console.Write("Ведите кол-во элементов в массиве строк или просто нажмите Enter(применится размер по умолчанию = 10 ):");
+            string in_size = Console.ReadLine();
+            if (in_size.Length > 0) { size = Convert.ToInt32(in_size); }
+
+            string[] myrows = new string[size];
+            Console.WriteLine($"Ведите масив строк из {size} елементов, или оставте пустым для генерации случайными значениями");
+            Console.WriteLine("Введите 1-й элемент");
+            string first = Console.ReadLine();
+            if (first != "")
+            {
+                myrows[0] = first;
+                for (int i = 1; i < size; i++)
+                {
+                    Console.WriteLine("Введите {0}-й элемент", i + 1);
+                    myrows[i] = Console.ReadLine();
+                }
+            }
+            else
+            {
+              myrows = CreateArray(size, min, max); 
+            }
             Console.WriteLine("Из cгенерированного массива строк, формирует новый массив из строк, длина которых меньше, либо равна 3 символам.");
             PrintArray(myrows);
             Console.Write(" -> ");
